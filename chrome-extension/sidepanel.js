e7848f9637dc775568cb09cd4ad8348ce581fb6c
Chris Irineo <chriskevini@gmail.com>
2025-11-16 01:01:47 -0800

    Remove unused escapeHtml function from sidepanel

--- chrome-extension/sidepanel.js
+++ chrome-extension/sidepanel.js
@@ -316,11 +316,3 @@ function showToast(message, type = 'info') {
     toast.classList.add('hidden');
   }, 3000);
 }
-
-// Escape HTML to prevent XSS
-function escapeHtml(text) {
-  if (!text) return '';
-  const div = document.createElement('div');
-  div.textContent = text;
-  return div.innerHTML;
-}

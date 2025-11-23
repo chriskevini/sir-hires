649cab07db022b116ba3a7bd0436308caa942b02
Chris Irineo <chriskevini@gmail.com>
2025-11-22 18:57:35 -0800

    fix: extract ResearchingView styles to shared component stylesheet for consistent rendering across entrypoints

--- src/entrypoints/job-details/views/researching-view.tsx
+++ src/entrypoints/job-details/views/researching-view.tsx
@@ -14,6 +14,7 @@ import { JobTemplatePanel } from '../components/JobTemplatePanel';
 import { ExtractionLoadingView } from '../components/ExtractionLoadingView';
 import { ExtractionErrorView } from '../components/ExtractionErrorView';
 import { MigrationPromptView } from '../components/MigrationPromptView';
+import './researching-view.css';
 
 interface Job {
   id: string;

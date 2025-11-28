0a8a63e660dfc41eefb533cc87bd9b334731352b
Chris Irineo <chriskevini@gmail.com>
2025-11-28 00:02:24 -0800

    fix: display knight mascot icon with transparent background

--- wxt.config.ts
+++ wxt.config.ts
@@ -21,6 +21,16 @@ export default defineConfig({
     host_permissions: ['http://localhost:*/*', 'http://127.0.0.1:*/*'],
     action: {
       default_title: 'Sir Hires - Open Sidepanel',
+      default_icon: {
+        16: 'icons/icon16.png',
+        48: 'icons/icon48.png',
+        128: 'icons/icon128.png',
+      },
+    },
+    icons: {
+      16: 'icons/icon16.png',
+      48: 'icons/icon48.png',
+      128: 'icons/icon128.png',
     },
   },
 });
